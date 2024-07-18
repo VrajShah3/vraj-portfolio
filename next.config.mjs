1e313076478209906414e43b5238d7b77cbc4f77
Vraj Shah <vhshah035@gmail.com>
2024-07-18 11:51:16 +0530

    added

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export',
-    basePath: '/https://github.com/VrajShah3/vraj-portfolio',
-    assetPrefix: '/https://github.com/VrajShah3/vraj-portfolio/',
-  }
-  
-  export default nextConfig;  
+const nextConfig = {};
+
+export default nextConfig;
